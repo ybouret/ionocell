72107da18686636e9b19bdd44deb0c6c46420ed8
Jeanne Le Clezio <jleclezio@pmjlc19521.local>
2025-04-14 11:07:38 +0200

    Definition de 3 classes : Grid, Specie, Reaction

--- python/ionocell.py
+++ python/ionocell.py
@@ -1,17 +1,62 @@
 """ IonoCell """
+import numpy as np
 
 
 class Grid:
     """ Grid for one space """
 
-    def __init__(self, _length):
-        self.length = _length
+    def __init__(self, _space, _species_list, _transport_meth, _pas_temps, _pas_espace, _concentration ):
+        
+        self.space = _space #xi
+        self.length = len(_space) #xi
+        self.species = _species_list #SPECIES
+        self.meth = _transport_meth #Type_mb
+        self.dt = _pas_temps
+        self.dx = _pas_espace
+        self.value = _concentration #TS_x1
 
     def show(self):
         """ show Grid parameters """
         print("length=", self.length)
+        print("species name =", self.species)
+        
+    # def add_time(self, t_value):
+    #     self.times.append(t_value)
+        
+# gridx1 = Grid(x1, ["Na","Cl"], "osmose implicite", delta_t, delta_x,  TCF_x1)
+
+class Specie:
+    """ params for each species """
+    
+    def __init__(self, _specie_name, _diff_coeff, _perm_coeff, _charge, _marker):
+        
+        self.name = _specie_name,
+        self.diff = _diff_coeff
+        self.perm = _perm_coeff
+        self.charge = _charge
+        self.marker = _marker
+    
+    def show(self):
+        print("specie name =" , self.name)
 
 
-grid = Grid(0.1)
+# test = Specie("Na", 1, 10, +1, '-d')
 
-grid.show()
+
+class Reaction:
+    """ params for reactions """
+    
+    def __init__(self, _TF, _reactif, _produit, _cst_eq):
+        
+        self.TF = _TF,
+        self.reac = _reactif
+        self.pdt = _produit
+        self.cst_eq = _cst_eq
+    
+    def show(self):
+        print("reaction ? " , self.TF)
+
+
+
+
+
