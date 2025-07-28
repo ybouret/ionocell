5645320619cf183292f85594e4abd94128512720
Jeanne Le Clezio <jeanne.leclezio@gmail.com>
2025-07-28 14:17:34 +0200

    resolution pb reaction : plus de np.copy

--- python/Functions/Reaction_functions.py
+++ python/Functions/Reaction_functions.py
@@ -42,31 +42,31 @@ def reaction_euler(list_SP, Grid_to_react, react_dict):
     
     NS = len(list_SP)
     
-    Grid_to_react = np.array(Grid_to_react)
+    Grid_to_react_t = [np.zeros_like(arr) for arr in Grid_to_react]
     
     SPECIES = []
     for sp in list_SP :
         SPECIES.append(sp.name[0])
-    
+
     for R in range (0, nb_react):
         reaction0 = react_dict[f"reaction{R}"]
         k_react = reaction0['cst_eq']
         
         REACTIF = []
         for reactif in reaction0['reactif']:
-            REACTIF.append(Grid_to_react[SPECIES.index(reactif),:])
+            REACTIF.append(Grid_to_react[SPECIES.index(reactif)])
             
         REACTIFS = np.prod(REACTIF, axis=0)
-            
+        
         for species in range(0, NS):
             sp = SPECIES[species]
             
             if sp in reaction0["reactif"]:
-                Grid_to_react[species] = Grid_to_react[species] - k_react*REACTIFS
+                Grid_to_react_t[species] = Grid_to_react[species] - k_react*REACTIFS
             elif sp in reaction0["produit"]:
-                Grid_to_react[species] = Grid_to_react[species] + k_react*REACTIFS
+                Grid_to_react_t[species] = Grid_to_react[species] + k_react*REACTIFS
 
-    return (Grid_to_react)
+    return (Grid_to_react_t)
 
 
 
